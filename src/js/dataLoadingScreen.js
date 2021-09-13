063d3a902f5368119b74036f465776463f041856
Karol Stawowski <stawowski.dev@gmail.com>
2021-09-13 20:59:39 +0200

    Change getting loading screen image method

--- src/js/dataLoadingScreen.js
+++ src/js/dataLoadingScreen.js
@@ -1,5 +1,9 @@
 import * as elements from "./variables/documentElements";
 
 export function dataLoadingScreen() {
-  elements.mainContent.innerHTML = `<div class="loading-screen"><img class="loading-image" src="src/assets/logo.png"></div>`;
+  elements.mainContent.innerHTML = `<div id="loading-screen" class="loading-screen"></div>`;
+  let img = document.createElement("img");
+  img.src = new URL("../assets/logo.png", import.meta.url);
+  img.className = "loading-image";
+  document.getElementById("loading-screen").appendChild(img);
 }
