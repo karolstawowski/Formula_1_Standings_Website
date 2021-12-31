9f1afc1f544ce80e6705e7febb5c65edb54b76ba
Karol Stawowski <stawowski.dev@gmail.com>
2021-12-31 15:29:54 +0100

    feat: all season are now available to display

--- src/js/sidebarButtonsCreator.js
+++ src/js/sidebarButtonsCreator.js
@@ -1,17 +1,19 @@
 export function createSidebarButtons() {
   const sidebarContent = document.getElementById("sidebar-button-wrapper");
-  for (let i = 22; i > -1; i--) {
+  for (let i = 2022; i > 1949; i--) {
     const sidebarItem = document.createElement("li");
     const sidebarButton = document.createElement("button");
     sidebarButton.classList.add("sidebar-button");
     sidebarButton.setAttribute("fetch-button", "");
-    if (i / 10 < 1) {
-      sidebarButton.id = "200" + i;
-      sidebarButton.innerHTML = "200" + i;
-    } else {
-      sidebarButton.id = "20" + i;
-      sidebarButton.innerHTML = "20" + i;
-    }
+    sidebarButton.id = i;
+    sidebarButton.innerHTML = i;
+    // if (i / 10 < 1) {
+    //   sidebarButton.id = "200" + i;
+    //   sidebarButton.innerHTML = "200" + i;
+    // } else {
+    //   sidebarButton.id = "20" + i;
+    //   sidebarButton.innerHTML = "20" + i;
+    // }
     sidebarItem.append(sidebarButton);
     sidebarContent.append(sidebarItem);
   }
